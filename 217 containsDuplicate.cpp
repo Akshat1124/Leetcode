5eab190438e98fd13236caae1214fafb3dc28573
AKSHAT SRIVASTAVA <akshatsrivastava1124@gmail.com>
2025-09-13 14:39:14 +0530

    optimal solution for contains duplicate

--- 217 containsDuplicate.cpp
+++ 217 containsDuplicate.cpp
@@ -10,3 +10,19 @@ public:
       return false;  
     }
 };
+
+// optimal O(n)
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+      unordered_set<int> s ;
+      for (int i : nums){
+        if (s.count(i)){
+          return true;
+        }
+        s.insert(i);
+      }
+      return false;  
+    }
+};
