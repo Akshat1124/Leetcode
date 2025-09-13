bcdbc50652b899d532788587613a3bd2ce23ccf9
AKSHAT SRIVASTAVA <akshatsrivastava1124@gmail.com>
2025-09-13 13:58:26 +0530

    217 contains duplicate

---
+++ sortingInArray.cpp
@@ -0,0 +1,10 @@
+// C++
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr = {5, 2, 9, 1};
+    sort(arr.begin(), arr.end()); // ascending
+    for (int x : arr) cout << x << " ";
+    return 0;
+}
